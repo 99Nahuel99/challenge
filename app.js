ec59415145b6ede47559aa35c308fe6df1ab8b4a
Nahuel <nahuelr354@gmail.com>
2024-08-27 16:45:27 -0300

    actualizando app

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function encriptar(){
 
 function desencriptar(){
     ocultarAdelante();
-    resultado.textContent =desencriptarTexto(recuperarTexto());
+    resultado.textContent = desencriptarTexto(recuperarTexto());
 }
 
 
@@ -31,52 +31,96 @@ parrafo.classList.add("ocultar");
 
 }
 function encriptarTexto(mensaje){
+   
     var texto1 = mensaje;
     var textoFinal="";
+
+     
     for(var i=0;i<texto1.length;i++){
-        if(texto1[i]=="a"){
+        texto1=texto1.toLocaleLowerCase();
+    
+        if(texto1[i]=='a'){
             textoFinal=textoFinal + "ai";
-        }
+        } else if(texto1[i] =="á"){
+            textoFinal=textoFinal +"ai";
+       }
+        
         else if(texto1[i] =="e"){
              textoFinal=textoFinal +"enter";
         }
+        else if(texto1[i] =="é"){
+            textoFinal=textoFinal +"enter";
+       }
         else if(texto1[i] =="i"){
             textoFinal=textoFinal + "ines";
        }
+       else if(texto1[i] =="í"){
+        textoFinal=textoFinal + "ines";
+   }
        else if(texto1[i] =="o"){
+            textoFinal=textoFinal + "ober";
+        }
+        else if(texto1[i] =="ó"){
             textoFinal=textoFinal + "ober";
         }
     else if(texto1[i] =="u"){
     textoFinal=textoFinal + "ufat";
-}
+    }
+    else if(texto1[i] =="ú"){
+        textoFinal=textoFinal + "ufat";
+        }
 else{
     textoFinal=textoFinal+texto1[i];
-}
-
-}
+    }
+    }
 return textoFinal;
 }
+
+
 function desencriptarTexto(mensaje){
     var texto1 = mensaje;
     var textoFinal="";
+   
+    texto1=texto1.toLocaleLowerCase();
+  
     for(var i=0;i<texto1.length;i++){
         if(texto1[i]=="a"){
             textoFinal=textoFinal + "a";
             i=i+1;
         }
+        else if(texto1[i] =="á"){
+            textoFinal=textoFinal +"a";
+            i=i+4;
+       }
         else if(texto1[i] =="e"){
              textoFinal=textoFinal +"e";
              i=i+4;
         }
+        else if(texto1[i] =="é"){
+            textoFinal=textoFinal +"e";
+            i=i+4;
+       }
         else if(texto1[i] =="i"){
             textoFinal=textoFinal + "i";
             i=i+3;
        }
+       else if(texto1[i] =="í"){
+        textoFinal=textoFinal + "i";
+        i=i+3;
+   }
        else if(texto1[i] =="o"){
             textoFinal=textoFinal + "o";
             i=i+3;
         }
+        else if(texto1[i] =="ó"){
+            textoFinal=textoFinal + "o";
+            i=i+3;
+        }
     else if(texto1[i] =="u"){
+    textoFinal=textoFinal + "u";
+    i=i+3;
+}
+else if(texto1[i] =="ú"){
     textoFinal=textoFinal + "u";
     i=i+3;
 }
